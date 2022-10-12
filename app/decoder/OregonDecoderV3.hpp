b1a0cf9a11bb7cceab5611a3ba407a9fa0cee280
Andrew McDonnell <bugs@andrewmcdonnell.net>
2022-10-12 17:24:57 +1030

    Import V3 decoder

---
+++ app/decoder/OregonDecoderV3.hpp
@@ -0,0 +1,48 @@
+class OregonDecoderV3 : public DecodeOOK {
+public:
+    OregonDecoderV3() {}
+
+    // add one bit to the packet data buffer
+    virtual void gotBit (char value) {
+        data[pos] = (data[pos] >> 1) | (value ? 0x80 : 00);
+        total_bits++;
+        pos = total_bits >> 3;
+        if (pos >= sizeof data) {
+            resetDecoder();
+            return;
+        }
+        state = OK;
+    }
+
+    virtual char decode (word width) {
+        if (200 <= width && width < 1200) {
+            byte w = width >= 700;
+            switch (state) {
+                case UNKNOWN:
+                    if (w == 0)
+                        ++flip;
+                    else if (32 <= flip) {
+                        flip = 1;
+                        manchester(1);
+                    } else
+                        return -1;
+                    break;
+                case OK:
+                    if (w == 0)
+                        state = T0;
+                    else
+                        manchester(1);
+                    break;
+                case T0:
+                    if (w == 0)
+                        manchester(0);
+                    else
+                        return -1;
+                    break;
+            }
+        } else {
+            return -1;
+        }
+        return  total_bits == 80 ? 1: 0;
+    }
+};
